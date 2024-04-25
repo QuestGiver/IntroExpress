76cc4f3f0b8e58cd9ef5452ae2a70f48f2510ace
Paul Gibbons <31017045+QuestGiver@users.noreply.github.com>
2024-04-24 23:19:28 -0700

    Add routing functionality
    Add a logger function,
    dynamic routing,
    routes folder,
    lots of comments on how all this works

---
+++ routes/users.js
@@ -0,0 +1,55 @@
+//this is a script is essentially a mini-app that can be injected into our main app.
+const express = require('express')
+const router = express.Router()//a router works the same as an app, with similar funtions like ".get" and ".post". 
+
+router
+.get("/", (req, res) =>{
+    res.send("User List")
+})
+
+router
+.post('/',(req,res) =>{//a post request for creating a new user
+    res.send('create user')
+   })
+
+router
+.get("/new",(req,res) =>{
+    res.send("User New Form")
+})
+
+router
+.route("/:id")//get, put, send, delte, etc. are all responces sent to the user.
+.get((req, res) =>{//router.route is essentially syntactic sugar that lets you chain together routing requests
+    console.log(req.user)
+    res.send(`User Get User with ID ${req.params.id}`)// in order to pass in code in a message you need to use "`" to denote your sending the return of something.
+    //"${}" is the syntax for passing in code inside a message
+    // we can create a dynamic parameter in a url can be done with ":". "id" is the varible that stands in for the code which determines the url and can be accessed with "." automatically
+    //Always place routes with static id's above dynamic ones, otherwise you can end up with a routing error. Express read code from top to bottom and anything after
+    // ":" counts as a valid string ID. SO "http://localhost:3000/users/new" would trigger this routes send request and also a route with a static route of "/new" below it.
+
+}).put((req, res) =>{
+    res.put(`Update user with ID ${req.params.id}`)//typically if you make a route, you'll also make a put route that allows you to update a user based on ID
+
+}).delete((req,res) =>{
+    res.send(`Delete User WITH ID ${req.params.id}`)
+})
+
+const users = [{name: "Kyle"}, {name: "Sally"}]
+router.param("id", (req, res, next, id) =>{
+    req.user = users[id]//<-- anytime we have an ID, get it from our user array
+    next()//<--this will call the next function in line which is going to be the first responce in .route("/:id") since this code only triggers on params with "id"
+})//runs any time it finds a parameter that matches the name you pass in
+//In english this code says:
+//Whenever you go to a route that has an "id" parameter i want you to run this code with a request, a responce, our next, and our id.
+//param will not run any other code unless we call the "next" function, causing an infinite load on the browser.
+//The way that the next funtion works is if you call this functionl, run the next funtion in line.
+//This is all because ".param" is essentially a type of "middleware". Middleware in express is stuff that run between a request being sent to a server and a responce being returned
+//to the user.
+//This means that middleware is ran BEFORE a responce is given to a user.
+//This also means that routing in general is middleware.
+//middleware is one of the most important concepts of Express
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(logger)//logger if placed here would run for everything in our middleware
 
-app.get('/', (req, res) =>{
-    console.log("Here")
-    res.render("index", {text: 'World'})
+app.get('/', logger, (req, res) =>{// you can pass anything into parameters to be run. In this instance, if logger was only used here, logger would run only for this get request.
+    res.render("index", {text: 'World'})//the second paramter "{text: 'World'}" is passed into the client from the server
 })
 
+//if app.use(logger) was moved here then the original url wont be printed because the above get request is defined before our logger function
+//So if you want a function to be used on all of your routes, always define it at the top of the page.
+//Alternatively, use them on individual end point for more precise execution
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)//the first parameter is where the route is mounted, somewhat like assigning a path to a file in a directory.
+
+function logger(req, re, next){
+    console.log(req.originalUrl)
+    next()
+}
+
 app.listen(3000)
+
+//middleware runs from top to bottom
